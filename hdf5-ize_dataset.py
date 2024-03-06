321be4a8a31a98b8173e800cdba0e298ab684cd7
haardie <gartmann@fjfi.cvut.cz>
2024-03-06 18:46:43 +0100

    Update hdf5-ize_dataset.py
    storing sparse matrix components instead of dense matrix

--- hdf5-ize_dataset.py
+++ hdf5-ize_dataset.py
@@ -3,6 +3,7 @@ import os
 import h5py
 import shutil
 import numpy as np
+from PIL import Image
 
 
 def traverse_dirs(root):
@@ -36,19 +37,23 @@ def traverse_dirs(root):
 def create_hdf5_struct(npz_dict, hdf5_path):
     with h5py.File(hdf5_path, 'w') as hdf5_file:
         for plane, classes in npz_dict.items():
-
             for cls, files in classes.items():
                 class_group_path = f"{plane}/{cls}"
                 class_group = hdf5_file.create_group(class_group_path)
-                label = 1 if cls == 'PDK' else 0
-                for file_path in files:
+
+                labels_dataset = class_group.create_dataset("labels", (len(files),), dtype='i8', compression='gzip')
+
+                # store components of a sparse matrix
+                for idx, file_path in enumerate(files):
                     basename = os.path.splitext(os.path.basename(file_path))[0]
                     csr_matrix = sp.load_npz(file_path)
-                    class_group.create_dataset(basename, data=csr_matrix.toarray())
-                    label_name = f"{basename}_L"
-                    class_group.create_dataset(label_name, data=np.array([label]))
 
-# ---------------
+                    for component in ['data', 'indices', 'indptr']:
+                        component_data = getattr(csr_matrix, component)
+                        dataset_name = f"{basename}_{component}"
+                        class_group.create_dataset(dataset_name, data=component_data, compression='gzip')
+
+                    labels_dataset[idx] = 1 if cls == 'PDK' else 0
 
 
 def create_dummy_data(root_dir):
@@ -57,8 +62,8 @@ def create_dummy_data(root_dir):
     planes = range(3)
     for plane in planes:
         for cls in classes:
-            for i in range(10):
-                random_matrix = np.random.binomial(1, 0.05, size=(1000, 1000))
+            for i in range(100):
+                random_matrix = np.random.binomial(1, 0.2, size=(1000, 1000))
                 sparse_matrix = sp.csr_matrix(random_matrix)
                 file_name = f'files_{cls}_0_dummy_plane{plane}_{i}.npz'
                 dir_path = os.path.join(root_dir, f'plane{plane}', cls)
@@ -66,31 +71,46 @@ def create_dummy_data(root_dir):
                 sp.save_npz(os.path.join(dir_path, file_name), sparse_matrix)
 
 
-def cleanup_dummy_data(root_dir):
+def load_sparse_matrix(hdf5_file, group_path, basename):
+    data = hdf5_file[f'{group_path}/{basename}_data'][:]
+    indices = hdf5_file[f'{group_path}/{basename}_indices'][:]
+    indptr = hdf5_file[f'{group_path}/{basename}_indptr'][:]
+    return sp.csr_matrix((data, indices, indptr))
+
+
+def cleanup_data(root_dir):
     if os.path.exists(root_dir):
         shutil.rmtree(root_dir)
 
 
-dummy_root = '/Users/hardie/dummy-npz-dataset/'
-create_dummy_data(dummy_root)
+if __name__ == '__main__':
 
-# test_path = '/Users/hardie/dummy-npz-dataset/plane0/AtmoNu/files_AtmoNu_0_dummy_plane0_1.npz'
-# test_mat = sp.load_npz(test_path)
-# print(test_mat)
+    dummy_root = '/Users/hardie/dummy-npz-dataset/'
+    # create_dummy_data(dummy_root)
 
-dummy_npz_dict = traverse_dirs(dummy_root)
-dummy_hdf5_path = 'dummy.hdf5'
-create_hdf5_struct(dummy_npz_dict, dummy_hdf5_path)
+    create_hdf5_struct(traverse_dirs(dummy_root), 'dummy.hdf5')
 
-with h5py.File(dummy_hdf5_path, 'r') as hdf:
-    def print_details(name, obj):
-        if isinstance(obj, h5py.Dataset):
-            print(f"Dataset: {name}")
-            print(f"Shape: {obj.shape}, Type: {obj.dtype}")
-            # print("Data:", obj[:])
-        else:
-            print(f"Group: {name}")
+    # with h5py.File('dummy.hdf5', 'r') as hdf:
+    #     def print_details(name, obj):
+    #         if isinstance(obj, h5py.Dataset):
+    #             print(f"Dataset (=file): {name}")
+    #             print(f"Shape: {obj.shape}, Type: {obj.dtype}")
+    #             # print("Data:", obj[:])
+    #         else:
+    #             print(f"Group (=folder): {name}")
+    #
+    #     hdf.visititems(print_details)
 
-    hdf.visititems(print_details)
-
-# cleanup_dummy_data(dummy_root)
+    # example matrix reconstruction & plotting
+    with h5py.File('dummy.hdf5', 'r') as hdf5_file:
+        # Load the sparse matrix
+        csr_matrix = load_sparse_matrix(hdf5_file, 'plane0/AtmoNu', 'files_AtmoNu_0_dummy_plane0_0')
+        dense = csr_matrix.todense()
+        min = dense.min()
+        max = dense.max()
+        norm_matrix = (dense - min) / (max - min) * 255
+        norm_matrix = norm_matrix.astype(np.uint8)
+        img = Image.fromarray(norm_matrix)
+        img.show()
+        
+    # cleanup_data(dummy_root)
