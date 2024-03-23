fee2560a5f2d6984e0adcebe850eb04dd1b2b544
haardie <gartmann@fjfi.cvut.cz>
2024-03-23 11:10:14 +0100

    added ROI extraction

--- hdf5-ize_dataset.py
+++ hdf5-ize_dataset.py
@@ -2,8 +2,6 @@ import h5py
 import numpy as np
 import uproot as ur
 import awkward as ak
-import torch
-import matplotlib.pyplot as plt
 
 
 def get_meta(branch):
@@ -28,6 +26,47 @@ def get_meta(branch):
     return image_meta
 
 
+def find_max_intensity_patch(arr, out_shape):
+    int_img = np.cumsum(np.cumsum(arr, axis=0), axis=1)
+
+    frame_sums = np.zeros((arr.shape[0] - out_shape[0] + 1, arr.shape[1] - out_shape[1] + 1))
+    for i in range(frame_sums.shape[0]):
+        for j in range(frame_sums.shape[1]):
+            total = int_img[i + out_shape[0] - 1, j + out_shape[1] - 1]
+            total -= int_img[i + out_shape[0] - 1, j - 1] if j > 0 else 0
+            total -= int_img[i - 1, j + out_shape[1] - 1] if i > 0 else 0
+            total += int_img[i - 1, j - 1] if (i > 0 and j > 0) else 0
+            frame_sums[i, j] = total
+
+    max_pos = np.unravel_index(np.argmax(frame_sums), frame_sums.shape)
+    max_int_patch = arr[max_pos[0]:max_pos[0] + out_shape[0],
+                        max_pos[1]:max_pos[1] + out_shape[1]]
+
+    return max_int_patch
+
+
+def center_nz_pattern(patch, out_shape):
+    nz_indices = np.transpose(np.nonzero(patch))
+    if nz_indices.size == 0:
+        return np.zeros(out_shape)
+
+    # new nz indices shifted to geometric center of the 'image'
+    cindices = nz_indices - np.mean(nz_indices, axis=0).astype(int) + np.array(out_shape) // 2
+    cpatch = np.zeros(out_shape)
+
+    # to filter out nz indices outside out_shape
+    valid_indices = (cindices[:, 0] >= 0) & (cindices[:, 0] < out_shape[0]) & \
+                    (cindices[:, 1] >= 0) & (cindices[:, 1] < out_shape[1])
+
+    cindices = cindices[valid_indices]
+    nz_values = patch[np.nonzero(patch)]
+    nz_values = nz_values[valid_indices]
+
+    cpatch[cindices[:, 0], cindices[:, 1]] = nz_values
+
+    return cpatch
+
+
 def get_comp_from_root(rootf, prec, next_):
     tree = rootf['image2d_tpc_tree']
     image_sup_branch = tree['_image_v']
@@ -37,16 +76,20 @@ def get_comp_from_root(rootf, prec, next_):
     image_meta = get_meta(image_sup_branch)
     col_count = image_sup_branch[image_meta['col_count']].array(entry_start=prec, entry_stop=next_)[0]
     row_count = image_sup_branch[image_meta['row_count']].array(entry_start=prec, entry_stop=next_)[0]
-    nz_indices_evt = {'0': {'x': [], 'y':  []},
+    nz_indices_evt = {'0': {'x': [], 'y': []},
                       '1': {'x': [], 'y': []},
                       '2': {'x': [], 'y': []}}
     nz_values_evt = {'0': [], '1': [], '2': []}
 
     for plane_idx in range(3):
         mdata = ak.to_numpy(linear_data[plane_idx]).reshape(col_count[plane_idx], row_count[plane_idx])
-        nz_indices = np.transpose(np.nonzero(mdata))
+        mdata[mdata < 0] = 0
+        max_int_patch = find_max_intensity_patch(mdata, (800, 200))
+        cmdata = center_nz_pattern(max_int_patch, (800, 800))
+
+        nz_indices = np.transpose(np.nonzero(cmdata))
         nz_x, nz_y = nz_indices[:, 0], nz_indices[:, 1]
-        nz_values = mdata[nz_x, nz_y]
+        nz_values = cmdata[nz_x, nz_y]
 
         nz_indices_evt[f'{plane_idx}']['x'] = nz_x
         nz_indices_evt[f'{plane_idx}']['y'] = nz_y
@@ -56,30 +99,7 @@ def get_comp_from_root(rootf, prec, next_):
     return nz_indices_evt, nz_values_evt
 
 
-def coo_to_torch(hdf5_path, event_id, plane_id):
-
-    image_key = f'evt{event_id}/plane{plane_id}'
-
-    with h5py.File(hdf5_path, 'r') as hdf5_file:
-        if image_key in hdf5_file:
-            dset = hdf5_file[image_key]
-
-            nz_x = dset['nz_x'][:]
-            nz_y = dset['nz_y'][:]
-            values = dset['value'][:]
-
-            values = torch.FloatTensor(values)
-            indices = torch.LongTensor([nz_x, nz_y])
-
-            rows, cols = max(nz_x) + 1, max(nz_y) + 1
-            sp_tensor = torch.sparse_coo_tensor(indices, values, torch.Size([rows, cols]))
-
-            return sp_tensor
-        else:
-            print(f"key {image_key} not found in HDF5 file")
-            return None
-
-def create_hdf5_from_root(rootf_path, hdf5_path, batch_size=128):
+def create_hdf5_from_root(rootf_path, hdf5_path, batch_size):
     dtype = np.dtype([
         ('nz_x', np.uint32),
         ('nz_y', np.uint32),
@@ -98,7 +118,7 @@ def create_hdf5_from_root(rootf_path, hdf5_path, batch_size=128):
                 end_idx = min((batch_num + 1) * batch_size, entries)
 
                 for evt_idx in range(start_idx, end_idx):
-                    nz_indices_evt, nz_values_evt = get_comp_from_root(rootf, evt_idx, evt_idx+1)
+                    nz_indices_evt, nz_values_evt = get_comp_from_root(rootf, evt_idx, evt_idx + 1)
                     event_group = batch_group.create_group(f"evt{evt_idx - start_idx + 1}")
 
                     for plane_idx in range(3):
@@ -108,55 +128,11 @@ def create_hdf5_from_root(rootf_path, hdf5_path, batch_size=128):
 
                         dset_name = f"evt{evt_idx + 1}/plane{plane_idx}"
 
-                        event_group.create_dataset(dset_name, data=components, compression='gzip', compression_opts=1, chunks=True)
-
-def print_hdf5_dirtree(hdf5_path):
-    with h5py.File(hdf5_path, 'r') as file:
-        def recurse_through_group(group, indent=0):
-            for key, item in group.items():
-                print('│   ' * indent + '├── ' + key)
-                if isinstance(item, h5py.Group):
-                    recurse_through_group(item, indent + 1)
-                elif isinstance(item, h5py.Dataset):
-
-                    if item.dtype.names:
-                        print('│   ' * (indent + 1) + '└── {} (shape: {}, dtype: {})'.format(key, item.shape, item.dtype))
-                        for name in item.dtype.names:
-                            print('│   ' * (indent + 2) + '├── {} (field of {})'.format(name, key))
-                    else:
-                        print('│   ' * (indent + 1) + '└── {} (shape: {}, dtype: {})'.format(key, item.shape, item.dtype))
-
-        recurse_through_group(file)
-
-
-def plot_im_from_hdf5(hdf5_path, event_id, plane_idx):
-
-    image_key = f'evt{event_id}/plane{plane_idx}'
-
-    with h5py.File(hdf5_path, 'r') as hdf5_file:
-        if image_key in hdf5_file:
-            dset = hdf5_file[image_key]
-            nz_x = dset['nz_x'][:]
-            nz_y = dset['nz_y'][:]
-            values = dset['value'][:]
-
-            rows, cols = max(nz_x) + 1, max(nz_y) + 1
-
-            densem = np.zeros((rows, cols))
-            densem[nz_x, nz_y] = values
-            
-            plt.imshow(densem, cmap='viridis', interpolation='nearest', aspect='auto')
-            plt.colorbar()
-            plt.tight_layout()
-            plt.show()
-        else:
-            print(f"key {image_key} not found in HDF5 file")
+                        event_group.create_dataset(dset_name, data=components, chunks=True, compression_opts=1,
+                                                   compression='gzip')
 
 
 if __name__ == '__main__':
     rootf_path = '/Users/hardie/research/ROOT/protondecay_hA_BodekRitchie_dune10kt_1x2x6_54474279_179_20220423T063923Z_gen_g4_detsim_reco_65804491_0_20230126T175412Z_reReco_larcv.root'
     hdf5_path = '/Users/hardie/sample_root.hdf5'
     create_hdf5_from_root(rootf_path, hdf5_path, batch_size=64)
-    print_hdf5_dirtree(hdf5_path)
-    # plot_im_from_hdf5(hdf5_path, 1, 2)
-    # sp_tensor = coo_to_torch(hdf5_path, 1, 2)
